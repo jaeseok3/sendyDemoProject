014d0ff36d1b6bd8e28670a3e0983326ee558f86
jaeyeong <201524443@pusan.ac.kr>
2019-12-27 17:35:39 +0900

    클릭리스너 구현

--- app/src/main/java/com/example/googlemapdemo/MapsActivity.kt
+++ app/src/main/java/com/example/googlemapdemo/MapsActivity.kt
@@ -9,10 +9,12 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.Marker
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    private var selectedMarker:Marker? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +23,25 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+    }
+
+    private val markerClickListener = object : GoogleMap.OnMarkerClickListener {
+        override fun onMarkerClick(marker: Marker?): Boolean {
+            if (marker == selectedMarker) {
+                selectedMarker = null
+                // Return true to indicate we have consumed the event and that we do not
+                // want the the default behavior to occur (which is for the camera to move
+                // such that the marker is centered and for the marker's info window to open,
+                // if it has one).
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(selectedMarker, 15.0f))
+                return true
+            }
+
+            selectedMarker = marker
+            // Return false to indicate that we have not consumed the event and that
+            // we wish for the default behavior to occur.
+            return false
+        }
     }
 
     /**
@@ -36,8 +57,21 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val FirstBusk = LatLng(35.1583167, 129.1577913)
-        mMap.addMarker(MarkerOptions().position(FirstBusk).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(FirstBusk))
+        val BuskDong = LatLng(35.205411, 129.077885)
+        val BuskHae = LatLng(35.158713, 129.160248)
+        val BuskPnu = LatLng(35.231028, 129.082287)
+        val BuskGwang = LatLng(35.153028, 129.118666)
+        mMap.addMarker(MarkerOptions().position(BuskDong).title("Marker in 동래"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(BuskDong))
+        mMap.addMarker(MarkerOptions().position(BuskHae).title("Marker in 해운대"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(BuskHae))
+        mMap.addMarker(MarkerOptions().position(BuskPnu).title("Marker in 부산대"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(BuskPnu))
+        mMap.addMarker(MarkerOptions().position(BuskGwang).title("Marker in 광안리"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(BuskGwang))
+
+        mMap.setOnMapClickListener { markerClickListener }
+
+
     }
 }
