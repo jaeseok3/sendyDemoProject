0d9ee53388d5596092f05bd3cfe979085ccb90b5
djdj7087 <59275331+djdj7087@users.noreply.github.com>
2019-12-30 09:59:38 +0900

    get Context

---
+++ app/src/main/java/com/example/sendymapdemo/app.kt
@@ -0,0 +1,18 @@
+package com.example.sendymapdemo
+
+import android.app.Application
+import android.content.Context
+
+class App : Application(){
+    companion object{
+        lateinit var instance: App
+        private set
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+
+    fun Context() : Context = applicationContext
+}
