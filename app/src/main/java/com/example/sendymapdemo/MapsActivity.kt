145adc1ffb6499b4483c237c2d3e9e59f03d2d68
jaeyeong <201524443@pusan.ac.kr>
2019-12-30 11:53:31 +0900

    이미지 띄우기 (동래) -> Adapter

--- app/src/main/java/com/example/sendymapdemo/MapsActivity.kt
+++ app/src/main/java/com/example/sendymapdemo/MapsActivity.kt
@@ -2,14 +2,15 @@ package com.example.sendymapdemo
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast.LENGTH_SHORT
 import android.widget.Toast.makeText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.GoogleMap.InfoWindowAdapter
-import com.google.android.gms.maps.GoogleMap.OnMapClickListener
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
@@ -36,6 +37,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             mMap.moveCamera(CameraUpdateFactory.newLatLng(marker?.position))
             mMap.animateCamera(CameraUpdateFactory.zoomTo(15f))
             makeText(App.instance.Context(), marker?.title, LENGTH_SHORT).show()
+            val adapter = CustomInfoWindowAdapter(this)
+            mMap.setInfoWindowAdapter(adapter)
             marker.showInfoWindow()
             true
         }
@@ -61,31 +64,5 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         /* must change to user's current position */
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(cityHallBus, 11.0f))
         mMap.setOnMarkerClickListener(markerClickListener)
-
-        mMap.setInfoWindowAdapter(object : InfoWindowAdapter {
-            // Use default InfoWindow frame
-            override fun getInfoWindow(marker: Marker): View? {
-                return null
-            }
-
-            // Defines the contents of the InfoWindow
-            override fun getInfoContents(marker: Marker): View { // Getting view from the layout file info_window_layout
-                val v: View = layoutInflater.inflate(R.layout.info_layout, null)
-                // Getting the position from the marker
-                val latLng = marker.position
-                // Getting reference to the TextView to set latitude
-                //val tvLat = v.findViewById<View>(R.id.tv_lat) as TextView
-                // Getting reference to the TextView to set longitude
-                //val tvLng = v.findViewById<View>(R.id.tv_lng) as TextView
-                // Setting the latitude
-                //tvLat.text = "Latitude:" + latLng.latitude
-                // Setting the longitude
-                //tvLng.text = "Longitude:" + latLng.longitude
-
-                //val img = v.findViewById<View>(R.id.dongraeImg)
-                // Returning the view containing InfoWindow contents
-                return v
-            }
-        })
     }
 }
