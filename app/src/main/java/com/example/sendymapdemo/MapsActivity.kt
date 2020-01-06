ba0da81a0e10906f5b0996312d182173c9828f66
djdj7087 <59275331+djdj7087@users.noreply.github.com>
2020-01-06 10:36:53 +0900

    naver map API some addition

--- app/src/main/java/com/example/sendymapdemo/MapsActivity.kt
+++ app/src/main/java/com/example/sendymapdemo/MapsActivity.kt
@@ -3,17 +3,26 @@ package com.example.sendymapdemo
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
-import com.naver.maps.map.MapFragment
-import com.naver.maps.map.NaverMap
-import com.naver.maps.map.OnMapReadyCallback
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.maps.model.LatLng
+import com.naver.maps.map.*
+import com.naver.maps.map.util.FusedLocationSource
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
-    private val fragmentManager = supportFragmentManager
-    private val mapFragment = fragmentManager.findFragmentById(R.id.map) as MapFragment?
-            ?: MapFragment.newInstance().also {
-                fragmentManager.beginTransaction().add(R.id.map, it).commit()
-            }
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
+    }
+
+    private lateinit var locationSource: FusedLocationSource
+
+    private var isFabOpen: Boolean = false
+    private lateinit var fabOpen: Animation
+    private lateinit var fabClose: Animation
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,9 +33,65 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 1)
         }
 
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+
+        fabOpen = AnimationUtils.loadAnimation(App.instance.Context(), R.anim.fab_open)
+        fabClose = AnimationUtils.loadAnimation(App.instance.Context(), R.anim.fab_close)
+
+        val fragmentManager = supportFragmentManager
+        val mapFragment = fragmentManager.findFragmentById(R.id.map) as MapFragment?
+                ?: MapFragment.newInstance().also {
+                    fragmentManager.beginTransaction().add(R.id.map, it).commit()
+                }
+
         mapFragment.getMapAsync(this)
     }
 
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if(locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)){
+            return
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
     override fun onMapReady(naverMap: NaverMap) {
+        val fab: View = findViewById(R.id.fab)
+        val currentLocation: View = findViewById(R.id.fab1)
+        val selectLocation: View = findViewById(R.id.fab2)
+
+        fab.setOnClickListener {
+            animation()
+        }
+        currentLocation.setOnClickListener { //두번째 버튼 눌렀을때 동작
+            animation()
+        }
+        selectLocation.setOnClickListener {  //첫번째 버튼 클릭했을때
+            animation()
+        }
+
+        naverMap.locationSource = locationSource
+        naverMap.locationTrackingMode = LocationTrackingMode.Follow
+        naverMap.locationOverlay.isVisible = true
+        naverMap.uiSettings.isLocationButtonEnabled = true
+    }
+
+    private fun animation(){
+        val currentLocation: View = findViewById(R.id.fab1) //지도상 원하는 위치 마커 찍기
+        val selectLocation: View = findViewById(R.id.fab2) //현재 위치 마커 찍기
+
+        if(isFabOpen){
+            currentLocation.startAnimation(fabClose)
+            selectLocation.startAnimation(fabClose)
+            currentLocation.isClickable = false
+            selectLocation.isClickable = false
+            isFabOpen = false
+        }
+        else{
+            currentLocation.startAnimation(fabOpen)
+            selectLocation.startAnimation(fabOpen)
+            currentLocation.isClickable = true
+            selectLocation.isClickable = true
+            isFabOpen = true
+        }
     }
 }
