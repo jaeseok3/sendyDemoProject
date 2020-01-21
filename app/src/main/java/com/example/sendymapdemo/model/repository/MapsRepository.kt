60d07b86a9daf005d0b786922850ce03338cf5f6
jaeyeong <201524443@pusan.ac.kr>
2020-01-21 14:20:07 +0900

    리포지토리 패턴 중간과정

--- app/src/main/java/com/example/sendymapdemo/model/repository/MapsRepository.kt
+++ app/src/main/java/com/example/sendymapdemo/model/repository/MapsRepository.kt
@@ -4,12 +4,14 @@ import android.util.Log
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
 import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.overlay.PathOverlay
 
 class MapsRepository:OnMapReadyCallback{
     var nMap:NaverMap? = null
     var markerStartPoint = Marker()
     var markerWayPoint = Marker()
     var markerGoalPoint = Marker()
+    var pathOverlay:PathOverlay = PathOverlay()
     var listener: (()->Unit)? = null
 
     override fun onMapReady(naverMap: NaverMap) {
